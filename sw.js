2742d8f5e8afd7520b14c6918f87cff40bc31328
sharpkathy123 <kathysharp@gmail.com>
2026-08-17 21:36:45 -0700

    Add service worker for offline support
    Implement service worker for offline caching of assets.

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'collage-builder-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Event: Save app files into offline cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event: Clean up old cache versions if updated
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event: Serve cached local files when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
