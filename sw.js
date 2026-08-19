90e96d44622e9cc6f3b9c6811043f3752727ec11
sharpkathy123 <kathysharp@gmail.com>
2026-08-18 17:16:27 -0700

    Update cache name with new timestamp

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker with Timestamped Cache Key
-const CACHE_NAME = 'grid-collage-cache-2026-08-18-1658';
+const CACHE_NAME = 'grid-collage-cache-2026-08-18-1710';
 
 const ASSETS_TO_CACHE = [
   './',
